89b76da94ef512c02442633ba4a89ff89e7aef1a
Guillaume Fazékas <guillaume.fazekas@gmail.com>
2021-08-07 22:06:37 +0200

    Affichage OK

--- app.js
+++ app.js
@@ -1,25 +1,71 @@
-const GameBoard = (() => {
+const gameBoard = (() => {
+
+  const squaresDOM = document.querySelectorAll(".square");
 
   var squares =
     ["X", "X", "X",
       "X", "X", "X",
       "X", "X", "X"];
 
-  return { squares };
+  var turn = Math.round(Math.random()) == 1 ? "x" : "o";
+
+  var nextTurn = turn == "x" ? "o" : "x";
+
+  const changeTurn = () => {
+    var a = turn;
+    var b = nextTurn;
+    turn = b;
+    nextTurn = a;
+    console.log(turn, nextTurn);
+  }
+
+  const getTurn = () => {
+    return turn;
+  }
+
+  const getNextTurn = () => {
+    return nextTurn;
+  }
+
+  return { squares, squaresDOM, getTurn, getNextTurn, changeTurn };
 
 })()
 
 const displayController = (() => {
 
-  const renderBoard = () => {
-    
+  const updateTurn = () => {
+    gameBoard.squaresDOM.forEach(
+      function (currentElement) {
+        if (!(currentElement.classList.contains("x") || currentElement.classList.contains("o"))) {
+          currentElement.classList.remove(`turn-${gameBoard.getNextTurn()}`);
+          currentElement.classList.add(`turn-${gameBoard.getTurn()}`);
+        }
+      }
+    )
   };
 
-  return { test1: 1, test2: 2 }
+  const initializeClickListeners = () => {
+    gameBoard.squaresDOM.forEach(
+      function (currentElement) {
+        currentElement.addEventListener('click', () => {
+          currentElement.classList.add(`${gameBoard.getTurn()}`);
+          currentElement.classList.remove(`turn-${gameBoard.getTurn()}`);
+          gameBoard.changeTurn();
+          updateTurn();
+        }, { once: true }
+        );
+      }
+    )
+  };
+
+
+  return { updateTurn, initializeClickListeners }
 })()
 
 const player = () => {
   return 1;
 }
 
-console.log(GameBoard.squares);
+displayController.updateTurn();
+
+displayController.initializeClickListeners();
