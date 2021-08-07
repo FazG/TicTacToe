b97f7b2df50fc305fb0e0e3d4d7b8c411b9e137f
Guillaume Fazékas <guillaume.fazekas@gmail.com>
2021-08-07 16:29:38 +0200

    Création du début du fichier js

---
+++ app.js
@@ -0,0 +1,25 @@
+const GameBoard = (() => {
+
+  var squares =
+    ["X", "X", "X",
+      "X", "X", "X",
+      "X", "X", "X"];
+
+  return { squares };
+
+})()
+
+const displayController = (() => {
+
+  const renderBoard = () => {
+    
+  };
+
+  return { test1: 1, test2: 2 }
+})()
+
+const player = () => {
+  return 1;
+}
+
+console.log(GameBoard.squares);
