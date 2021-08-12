67d40db96a397ef1f9880af0199c718792b5885b
Guillaume Fazékas <guillaume.fazekas@gmail.com>
2021-08-12 18:32:08 +0200

    Version fonctionelle

--- app.js
+++ app.js
@@ -1,11 +1,68 @@
 const gameBoard = (() => {
 
+  var gameIsFinished = false;
+
+  const getGameIsFinished = () => {
+    return gameIsFinished;
+  }
+
+  const toggleGameIsFinished = () => {
+    gameIsFinished = !gameIsFinished;
+  }
+
   const squaresDOM = document.querySelectorAll(".square");
 
-  var squares =
-    ["X", "X", "X",
-      "X", "X", "X",
-      "X", "X", "X"];
+  var xSquares = [];
+
+  var oSquares = [];
+
+  const consoleLogSquares = () => {
+    console.log("X");
+    console.log(xSquares);
+    console.log("O");
+    console.log(oSquares);
+  }
+
+  const winningCombinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]];
+
+  const checkWin = () => {
+    for (combination of winningCombinations) {
+      if (xSquares.includes(combination[0]) && xSquares.includes(combination[1]) && xSquares.includes(combination[2])) {
+        document.querySelector("body").classList.add("xWinLight");
+        document.querySelector("h1").classList.add("xWinDark");
+        document.querySelector("h2").textContent = "X wins !"
+        document.querySelector("#winDiv").style.backgroundColor = 'rgba(50, 77, 197, 0.76)';
+        document.querySelector("button").style.backgroundColor = 'rgb(40, 67, 190)';
+        document.querySelector("#winDiv").style.display = 'flex';
+        for (var i = 0; i < 9; i++) {
+          squaresDOM[i].classList.add("xWinBorderDark", "winSquare");
+        }
+        gameBoard.toggleGameIsFinished();
+        return;
+      }
+      else if (oSquares.includes(combination[0]) && oSquares.includes(combination[1]) && oSquares.includes(combination[2])) {
+        document.querySelector("body").classList.add("oWinLight");
+        document.querySelector("h1").classList.add("oWinDark");
+        document.querySelector("h2").textContent = "O wins !"
+        document.querySelector("#winDiv").style.backgroundColor = 'rgba(216, 100, 32, 0.76)';
+        document.querySelector("button").style.backgroundColor = 'rgba(206, 100, 13, 0.993)';
+        document.querySelector("#winDiv").style.display = 'flex';
+        for (var i = 0; i < 9; i++) {
+          squaresDOM[i].classList.add("oWinBorderDark", "winSquare");
+        }
+        gameBoard.toggleGameIsFinished();
+        return;
+      }
+    }
+  }
 
   var turn = Math.round(Math.random()) == 1 ? "x" : "o";
 
@@ -16,18 +73,24 @@ const gameBoard = (() => {
     var b = nextTurn;
     turn = b;
     nextTurn = a;
-    console.log(turn, nextTurn);
-  }
+  };
 
   const getTurn = () => {
     return turn;
-  }
+  };
 
   const getNextTurn = () => {
     return nextTurn;
-  }
+  };
 
-  return { squares, squaresDOM, getTurn, getNextTurn, changeTurn };
+  const resetGameBoard = () => {
+    xSquares.length = 0;
+    oSquares.length = 0;
+    turn = Math.round(Math.random()) == 1 ? "x" : "o";
+    nextTurn = turn == "x" ? "o" : "x";
+  };
+
+  return { xSquares, oSquares, squaresDOM, getTurn, getNextTurn, changeTurn, winningCombinations, checkWin, getGameIsFinished, toggleGameIsFinished, resetGameBoard, consoleLogSquares };
 
 })()
 
@@ -48,18 +111,52 @@ const displayController = (() => {
     gameBoard.squaresDOM.forEach(
       function (currentElement) {
         currentElement.addEventListener('click', () => {
-          currentElement.classList.add(`${gameBoard.getTurn()}`);
-          currentElement.classList.remove(`turn-${gameBoard.getTurn()}`);
-          gameBoard.changeTurn();
-          updateTurn();
+          if (!gameBoard.getGameIsFinished()) {
+            var turn = gameBoard.getTurn();
+            if (turn === "x") {
+              gameBoard.xSquares.push(parseInt(currentElement.dataset.index));
+            }
+            else if (turn === "o") {
+              gameBoard.oSquares.push(parseInt(currentElement.dataset.index));
+            }
+            currentElement.classList.add(`${turn}`);
+            currentElement.classList.remove(`turn-${turn}`);
+            gameBoard.changeTurn();
+            updateTurn();
+            gameBoard.consoleLogSquares();
+            gameBoard.checkWin();
+          }
         }, { once: true }
         );
       }
     )
   };
 
+  const initializeButton = () => {
+    document.querySelector("button").addEventListener('click', () => {
+      gameBoard.resetGameBoard();
+      resetDisplay();
+    })
+  };
 
-  return { updateTurn, initializeClickListeners }
+  const resetDisplay = () => {
+    document.querySelector("body").classList.remove("xWinLight", "oWinLight");
+    document.querySelector("h1").classList.remove("xWinDark", "oWinDark");
+    document.querySelector("#winDiv").style.display = 'none';
+    for (var i = 0; i < 9; i++) {
+      gameBoard.squaresDOM[i].click();
+      gameBoard.squaresDOM[i].classList.remove("xWinBorderDark", "oWinBorderDark", "winSquare");
+      gameBoard.squaresDOM[i].classList.remove("x");
+      gameBoard.squaresDOM[i].classList.remove("o");
+      gameBoard.squaresDOM[i].classList.remove("turn-x");
+      gameBoard.squaresDOM[i].classList.remove("turn-o");
+    }
+    gameBoard.toggleGameIsFinished();
+    initializeClickListeners();
+    updateTurn();
+  };
+
+  return { updateTurn, initializeClickListeners, resetDisplay, initializeButton }
 })()
 
 const player = () => {
@@ -67,5 +164,5 @@ const player = () => {
 }
 
 displayController.updateTurn();
-
 displayController.initializeClickListeners();
+displayController.initializeButton();
